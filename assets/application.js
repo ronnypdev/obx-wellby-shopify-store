c961c894ce327b60359677f312caaa1f84a11646
Ronny Perez <ronnypdev@gmail.com>
2020-02-04 23:01:52 -0500

    fix js bug

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,7 @@
 // Put your applicaiton javascript here
+$(document).ready(function() {
+  console.log('is working!');
+  $(document).on('click', '.js-quantity-button', function(event) {
+    alert('button confirmed');
+  });
+});
