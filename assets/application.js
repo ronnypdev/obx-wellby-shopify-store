16d51d1a3f52efebda464552b76d16736edb8260
Ronny Perez <ronnypdev@gmail.com>
2020-02-05 09:05:46 -0500

    Making the Quantity Component Functional

--- assets/application.js
+++ assets/application.js
@@ -1,7 +1,19 @@
 // Put your applicaiton javascript here
-$(document).ready(function() {
-  console.log('is working!');
-  $(document).on('click', '.js-quantity-button', function(event) {
-    alert('button confirmed');
+$(document).ready(function () {
+  $(document).on('click', '.js-quantity-button', function (event) {
+    // Targeting elements
+    let $button = $(this);
+    let $form = $button.closest('form');
+    let $quantity = $form.find('.js-quantity-field');
+    let quantityValue = parseInt($quantity.val());
+    let max = $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;
+
+    if ($button.hasClass('plus')) {
+      // do something for plus click
+      $quantity.val(quantityValue + 1);
+    } else if ($button.hasClass('minus')) {
+      // do something for minus click
+      $quantity.val(quantityValue - 1);
+    }
   });
 });
