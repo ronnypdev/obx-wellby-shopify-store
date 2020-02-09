34f05bfbdefc5af739667eb41bd7a41621253555
Ronny Perez <ronnypdev@gmail.com>
2020-02-08 20:14:51 -0500

    Hooking into the Quantity Change Event

--- assets/application.js
+++ assets/application.js
@@ -10,10 +10,26 @@ $(document).ready(function () {
 
     if ($button.hasClass('plus')) {
       // do something for plus click
-      $quantity.val(quantityValue + 1);
+      $quantity.val(quantityValue + 1).change();
     } else if ($button.hasClass('minus')) {
       // do something for minus click
-      $quantity.val(quantityValue - 1);
+      $quantity.val(quantityValue - 1).change();
+    }
+  });
+
+  $(document).on('change', '.js-quantity-field', function (event) {
+    let $field = $(this);
+    let $form = $field.closest('form');
+    let $quantityText = $form.find('.js-quantity-text');
+    let shouldDisableMinus = parseInt(this.value) === 1;
+    let $minusButton = $form.find('.js-quantity-button.minus');
+
+    $quantityText.text(this.value);
+
+    if (shouldDisableMinus) {
+      $minusButton.prop('disabled', true);
+    } else if ($minusButton.prop('disabled') === true) {
+      $minusButton.prop('disabled', false);
     }
   });
 });
